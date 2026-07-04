d6a51f99776d2c86fbab66af93e40a0e5590961f
Patrickowlstereo <469905744@qq.com>
2026-07-04 23:35:19 +0800

    feat(api): spring boot scaffold with jpa, security, flyway, jjwt

---
+++ api/src/main/java/com/gmnl/orientation/OrientationApplication.java
@@ -0,0 +1,11 @@
+package com.gmnl.orientation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(OrientationApplication.class, args);
+  }
+}
